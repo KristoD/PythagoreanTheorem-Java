f0bf2e7af3ec8420a9469630607df7090c4620f2
Chris <chrisdedeian@gmail.com>
2018-08-10 13:18:57 -0700

    Initial commit

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean calculation = new Pythagorean();
+        double result = calculation.calculateHypotenuse(45, 60);
+        System.out.println(result);
+    }
+}
